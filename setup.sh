165aa98754fd160ed378658927cf330a52a964ad
demithdj <anushadilrukshini7@gmail.com>
2026-05-31 12:50:01 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+# !/bin/bash
 
 # Termux Web Remote Control Setup Script
 echo "🚀 Termux Web Remote Control Setup"
@@ -7,26 +7,30 @@ echo ""
 
 # Update packages
 echo "📦 Updating packages..."
-pkg update -y
+pkg update -y && pkg upgrade -y
 
 # Install Python
 echo "🐍 Installing Python..."
 pkg install python -y
 
 # Install required Python packages
-echo "📚 Installing python server..."
-pkg install nodejs -y && pkg install nodejs-lts && pkg install android-tools -y
+echo "📚 Installing python server system..."
+pkg install nodejs -y && pkg install nodejs-lts -y && pkg install android-tools -y 
+# Setup the server
+echo " ⚙️⚙️ Setup the server system"
+git clone https://github.com/NetrisTV/ws-scrcpy.git && cd ws-scrcpy && npm install && cd ~/Remote
 
 echo ""
 echo "✅ Setup complete!"
 echo ""
+echo "⚙️ To connect ADB"
+echo "   adb start-server"
+echo "   adb connect localhost:5555"
+echo ""
 echo "📱 To run the server:"
-echo "   python3 termux_web_server.py"
+echo "   python termux_web_server.py"
 echo ""
-echo "🌐 To find your IP address:"
-echo "   ifconfig | grep 'inet '"
-echo ""
-echo "💻 Then open in Chrome:"
+echo "💻 Then open in Chrome (The ip address will be show in run server):"
 echo "   http://YOUR_IP:5000"
 echo ""
 echo "⚠️  WARNING: This server is not encrypted."
