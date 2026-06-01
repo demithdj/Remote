358bd3b3e2fc460a63e9bad537345b34f16b9c8f
demithdj <anushadilrukshini7@gmail.com>
2026-06-01 17:03:51 +0530

    Update setup script to use Node.js instead of Python
    Removed Python installation and updated server run command to Node.js.

--- setup.sh
+++ setup.sh
@@ -9,26 +9,20 @@ echo ""
 echo "📦 Updating packages..."
 pkg update -y && pkg upgrade -y
 
-# Install Python
-echo "🐍 Installing Python..."
-pkg install python -y
-
 # Install required Python packages
 echo "📚 Installing python server system..."
 pkg install nodejs -y && pkg install nodejs-lts -y && pkg install android-tools -y 
 # Setup the server
 echo " ⚙️⚙️ Setup the server system..."
-git clone https://github.com/NetrisTV/ws-scrcpy.git && cd ws-scrcpy && npm install && cd ~/Remote
+npm init -y && npm install
 
 echo ""
 echo "✅ Setup complete!"
 echo ""
-echo "⚙️ To connect ADB"
-echo "   adb start-server"
 echo "   adb connect localhost:5555"
 echo ""
 echo "📱 To run the server"
-echo "   python Termux_Remote_Server.py"
+echo "   node Termux_Remote_Server.js"
 echo ""
 echo "💻 Then open in Chrome (The ip address will be show in running server)"
 echo "   http://YOUR_IP:5000"
